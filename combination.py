5a62564b207045010def01ce9b74cda92ca71cfc
dannyso16 <osatoshi.11@gmail.com>
2019-03-29 22:31:26 +0900

    Add: other implementation
    前処理をして．クエリごとにO(1)で答えを返す

--- combination.py
+++ combination.py
@@ -4,9 +4,9 @@
 # a^(-1) ≡ a^(p-2) (mod p), p is prime number
 # 割り算 -> 逆元の掛け算に変形させる
 
-
 MOD = 10**9 + 7
 def comb_fermat(n:int, r:int)->int:
+    # Fermat's little theorem: O(r)
     # return nCr (mod MOD)
     if r > n-r: return comb(n, n-r)
     mul,div = 1,1
@@ -21,11 +21,51 @@ def comb_fermat(n:int, r:int)->int:
 
 # python らしいごり押し (あまり大きくないときに使おう)
 from math import factorial
-def comb(n:int, r:int)->int:
+def comb_naive(n:int, r:int)->int:
     ret = factorial(n) // (factorial(n-r)*factorial(r))
     return ret % MOD
+
+# com[2000][2000]
+com = [[0]*2000 for _ in range(2000)]
+com[0][0] = 1
+def calc_comb():
+    # k,n <= 2000
+    # dp (Pascal's triangle): O(n*k)
+    global com
+    for i in range(1, len(com)):
+        com[i][0] = 1
+        for j in range(1, len(com)):
+            com[i][j] = com[i-1][j-1] + com[i-1][j]
+            com[i][j] %= MOD
+
+
+# 逆元を前計算(O(MAX_N))しておくことでクエリをO(1)で返す
+MAX = 10**5
+fac = [0]*MAX  # fac[n]:  (n!) mod p
+finv = [0]*MAX # finv[n]: (n!)^-1 mod p
+inv = [0]*MAX  # inv[n]:  (n)^-1 mod -p
+
+def comb_init():
+    global fac, finv, inv
+    fac[0] = fac[1] = 1
+    finv[0] = finv[1] = 1
+    inv[1] = 1
+    for i in range(2, MAX):
+        fac[i] = fac[i-1] * i % MOD
+        inv[i] = MOD - inv[MOD%i] * (MOD//i)%MOD
+        finv[i] = finv[i-1] * inv[i] % MOD
+
+def comb(n:int, r:int)->int:
+    global fac, finv
+    if n < r: return 0
+    if n < 0 or r < 0: return 0
+    return fac[n] * (finv[r] * finv[n-r] % MOD) % MOD
 
 
 if __name__ == '__main__':
     print(comb_fermat(10, 3))
+    print(comb_naive(10, 3))
+    comb_init()
     print(comb(10, 3))
+    calc_comb()
+    print(com[10][3])
